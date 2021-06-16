ade959785532445a494db02f168ce893c2546359
Hamza Awan <hamza.awan@3-tree.com>
2021-06-16 23:12:34 +0500

    Side bar

--- src/components/dashboard/RightSideBar.jsx
+++ src/components/dashboard/RightSideBar.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export default function RightSideBar() {
+export default function RightSideBar(props) {
+
+    const leftIcons = [
+         {val: <i class="fas fa-exchange-alt"></i> ,  name: 'Deals' },
+         {val: <i class="fas fa-bolt"></i>,  name: 'Trends' },
+         {val: <i class="fas fa-users"></i>, name: 'Social' },
+    ];
+
+    const [ rightSide, setRightSide ] = useState(false);
+
+    const sideBarIcon = ( goTo ) => {
+        // history.push(goTo);
+    }
+
+    const rightSideBar = () => {
+        setRightSide(rightSide => !rightSide);
+
+        if(rightSide){
+            props.rightSidePanel(true);
+        }else{
+            props.rightSidePanel(false);
+        }
+    }
+
     return (
-        <div className="right-sdie-bar-conrainer">
-
+        <div className="left-sdie-bar-conrainer">
+            
+            <div className="icons-left-bar">
+                {leftIcons?.map( (item)=> (
+                    <div className={`icon-view icon-upper-half `}  
+                        onClick={() => rightSideBar() }
+                        >
+                        {item?.val}
+                        <div className="route-names">
+                            {item?.name}
+                        </div>
+                    </div>
+                ) )
+                }
+            </div>
+            
         </div>
     )
 }
